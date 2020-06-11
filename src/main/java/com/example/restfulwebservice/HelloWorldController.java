54cc3f59dc20df57cd4ecaf6f267c5700c168d68
AngryLuna <wsqa1231@gmail.com>
2020-06-11 16:47:03 +0900

    path variable 사용하는 API 추가

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,12 +23,24 @@ public class HelloWorldController {
 
     /**
      * method : GET
-     * endpoint : /hello-world
+     * endpoint : /hello-world-bean
      *
      * @return HelloWorldBean
      */
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    /**
+     * method : GET
+     * endpoint : /hello-world-bean/path-variable/{name}
+     *
+     * @param name String
+     * @return HelloWorldBean
+     */
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable final String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
