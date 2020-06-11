e9c4d76e630c9c92a747d2453116081e6f79d5e8
AngryLuna <wsqa1231@gmail.com>
2020-06-11 15:43:15 +0900

    테스트용 HelloWorld Controller 추가

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    /**
+     * method : GET
+     * endpoint : /hello-world
+     *
+     * @return String
+     */
+//    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
