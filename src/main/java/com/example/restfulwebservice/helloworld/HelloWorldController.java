dcc5a2a9531760a45c009830133511544f9c43e4
AngryLuna <wsqa1231@gmail.com>
2020-06-17 13:13:58 +0900

    다국어 처리

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,14 +1,22 @@
 package com.example.restfulwebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * 테스트용 RestController
  */
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     /**
      * method : GET
      * endpoint : /hello-world
@@ -43,4 +51,16 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(@PathVariable final String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    /**
+     * method : GET
+     * endpoint : /hello-world-internationalized
+     *
+     * @return
+     */
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) final Locale locale) {
+        return this.messageSource.getMessage("greeting.message", null, locale);
+    }
+
 }
