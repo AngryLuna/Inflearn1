ac4fbce4f1d86d928677a7c75be1145f6f2fc55c
AngryLuna <wsqa1231@gmail.com>
2020-06-19 16:54:40 +0900

    SWAGGER 라이브러리 및 설정 추가, HATEOAS 의존성 충돌 해결

--- src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -6,12 +6,13 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Swagger 설정
@@ -19,7 +20,15 @@ import java.util.List;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    private static final Contact DEFAULT_CONTACT = new Contact("AngryLuna", "http://localhost:8080", "wsqa1231@gmail.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Test Api Title", "My User Management REST API Service", "1.0",
+            "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
     /**
+     * HATEOAS 의존성 충돌 해결을 위한 코드
      * https://qastack.kr/programming/58431876/spring-boot-2-2-0-spring-hateoas-startup-issue
      *
      * @return
@@ -32,8 +41,16 @@ public class SwaggerConfig {
 
     }
 
+    /**
+     * SWAGGER 설정
+     *
+     * @return
+     */
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "회원 상세 정보")
 /**
  * 회원 정보 V1
  */
@@ -18,12 +21,16 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "이름은 2글자 이상 입력해주세요") // 최소 2글자 이상 제한
+    @ApiModelProperty(notes = "회원 이름을 입력해주세요")
     private String name;
 
     @Past // 과거의 날짜 제한
+    @ApiModelProperty(notes = "회원 등록일을 입력해주세요")
     private Date joinDate;
 
+    @ApiModelProperty(notes = "회원 비밀번호를 입력해주세요")
     private String password;
 
+    @ApiModelProperty(notes = "회원 주민번호를 입력해주세요")
     private String ssn;
 }
