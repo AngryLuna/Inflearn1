75229b0311a818f217712083b257bb853160100d
AngryLuna <wsqa1231@gmail.com>
2020-06-22 14:59:15 +0900

    h2 console 페이지 접근시 인증 필요없도록 설정

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -10,6 +11,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * h2-console 하위 경로에 대하여 무조건 허용
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
     @Autowired
     public void configureGlobal(final AuthenticationManagerBuilder builder) throws Exception {
         builder.inMemoryAuthentication()
